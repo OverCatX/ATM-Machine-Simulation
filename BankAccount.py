c9f9b020908f4366f4ab06d115af0440b76407c2
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-10-14 02:15:43 +0700

    created initial metohd

---
+++ BankAccount.py
@@ -0,0 +1,8 @@
+class BankAccount:
+
+    def __init__(self, account_number: int, account_holder: str, account_balance: float):
+        self._account_number = account_number
+        self._account_holder = account_holder
+        self._account_balance = account_balance
+
+
