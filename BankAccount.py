165c0b8b7d451d84bc23f3921f9698b2e6821e29
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-10-14 02:24:20 +0700

    changed method name

--- BankAccount.py
+++ BankAccount.py
@@ -5,24 +5,24 @@ class BankAccount:
         self._account_holder = account_holder
         self._account_balance = account_balance
 
-    def getAccountNumber(self) -> int:
+    def getNumber(self) -> int:
         return self._account_number
 
-    def getAccountHolder(self) -> str:
+    def getHolder(self) -> str:
         return self._account_holder
 
-    def getAccountBalance(self) -> float:
+    def getBalance(self) -> float:
         return round(self._account_balance, 2)
 
     def deposit(self, amount) -> None:
         if amount <= 0:
             print('Please type positive integer')
         self._account_balance += amount
-        print(f'Deposit successful! Your new balance is: {self.getAccountBalance()}')
+        print(f'Deposit successful! Your new balance is: {self.getBalance()}')
 
     def withdraw(self, amount) -> None:
-        if amount < self.getAccountBalance():
+        if amount < self.getBalance():
             print(f'Insufficient balance to withdraw')
         self._account_balance -= amount
-        print(f'Withdrawal successful! Your new balance is: ${self.getAccountBalance()}')
+        print(f'Withdrawal successful! Your new balance is: ${self.getBalance()}')
 
