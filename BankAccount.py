0f803e11ede43ef383062a4eb1a280781230e400
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-10-14 02:23:50 +0700

    added methods

--- BankAccount.py
+++ BankAccount.py
@@ -5,4 +5,24 @@ class BankAccount:
         self._account_holder = account_holder
         self._account_balance = account_balance
 
+    def getAccountNumber(self) -> int:
+        return self._account_number
+
+    def getAccountHolder(self) -> str:
+        return self._account_holder
+
+    def getAccountBalance(self) -> float:
+        return round(self._account_balance, 2)
+
+    def deposit(self, amount) -> None:
+        if amount <= 0:
+            print('Please type positive integer')
+        self._account_balance += amount
+        print(f'Deposit successful! Your new balance is: {self.getAccountBalance()}')
+
+    def withdraw(self, amount) -> None:
+        if amount < self.getAccountBalance():
+            print(f'Insufficient balance to withdraw')
+        self._account_balance -= amount
+        print(f'Withdrawal successful! Your new balance is: ${self.getAccountBalance()}')
 
