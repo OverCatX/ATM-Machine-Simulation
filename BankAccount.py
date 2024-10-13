444448065aa2b508b232375348bc24e969e7df33
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-10-14 02:28:49 +0700

    added account_pin attribute

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,12 @@
 class BankAccount:
 
-    def __init__(self, account_number: int, account_holder: str, account_balance: float):
+    def __init__(self, account_number: int
+                 , account_holder: str
+                 , account_pin: int
+                 , account_balance: float):
         self._account_number = account_number
         self._account_holder = account_holder
+        self._account_pin = account_pin
         self._account_balance = account_balance
 
     def getNumber(self) -> int:
