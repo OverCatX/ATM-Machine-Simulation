623a43b1904714da0d062a2d6236bdd0f6e2862c
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-10-14 18:48:25 +0700

    updated method

--- Atm.py
+++ Atm.py
@@ -5,7 +5,7 @@ class Atm:
 
     def __init__(self, account_data='accounts.json'):
         self.account_data = account_data
-        self.acounts = self.load_data()
+        self.accounts = self.load_data()
 
     def load_data(self):
         try:
@@ -18,16 +18,21 @@ class Atm:
         with open(self.account_data, 'w') as data:
             json.dump(self.account_data, data)
 
-    def createAccount(self, acc_number, acc_holder, acc_pin):
-        if acc_number in self.acounts:
+    def createAccount(self, acc_number, acc_holder, acc_pin) -> None:
+        if acc_number in self.accounts:
             print('This account has exists')
-        self.acounts[acc_number] = {
+        self.accounts[acc_number] = {
             'holder': acc_holder,
             'pin': acc_pin,
             'balance': 0
         }
         self.saveData()
-        print(f"Account's number: {acc_number} has been created.")
+        print(f"Account's number: [{acc_number}] has been created.")
 
-    def authenticate(self, acc_number, acc_pin):
+    def authenticate(self, acc_number, acc_pin) -> None:
+        if acc_number not in self.accounts:
+            print(f"Account's number [{acc_number}] doesn't exists.")
+        if self.accounts[acc_number]['pin'] != acc_pin:
+            print(f'Password not correct.')
         
+
